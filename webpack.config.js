00e650ec7e594f50402e5828f5808c00d2a3d7ec
Thiago Ferreira <thiagomf712@gmail.com>
2024-06-10 20:35:17 -0300

    Add assets and styles variables

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
-      favicon: path.resolve(__dirname, 'src', 'assets', 'present.svg'),
+      favicon: path.resolve(__dirname, 'src', 'assets', 'favicon.svg'),
     }),
     new CopyWebpackPlugin({
       patterns: [
