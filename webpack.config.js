f8ed7d9534e5b53d66a7a6f81ba3d5d299167608
Thiago Ferreira <thiagomf712@gmail.com>
2024-06-12 19:51:42 -0300

    search area layout

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
+      template: path.resolve(__dirname, 'index.html'),
       favicon: path.resolve(__dirname, 'src', 'assets', 'favicon.svg'),
     }),
     new CopyWebpackPlugin({
